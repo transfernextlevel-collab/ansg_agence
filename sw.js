9e0c018b20feda040b4401260ee3944a38eb46c6
Transfer NL <transfernextlevel@gmail.com>
2026-04-19 23:20:27 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+/* SW - Telephone/transfer-parceiro - source racine: Transfer parceiro .html */
+self.addEventListener('install', function (e) {
+  self.skipWaiting();
+});
+self.addEventListener('activate', function (e) {
+  e.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', function (e) {
+  e.respondWith(fetch(e.request));
+});
